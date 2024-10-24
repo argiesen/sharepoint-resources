6e39bd797e1ff9c7599ce3f1f4f4dc25677fb339
Andy Giesen <argiesen@gmail.com>
2024-10-23 23:49:27 -0700

    Update sharepoint-get-file-version-count.ps1

--- sharepoint-get-file-version-count.ps1
+++ sharepoint-get-file-version-count.ps1
@@ -1,39 +1,53 @@
-# Import the PnP PowerShell module
-Import-Module PnP.PowerShell
+# Created by: CompuNet Inc
+# Authors: Andy Giesen <agiesen@compunet.biz>
+# Last Modified October 23, 2024
 
-# Connect to SharePoint Online
-$tenantAdminUrl = "M365x97415188.sharepoint.com"
-$clientId = "c00e64bc-4761-4c36-b358-3119fea350e5"
-Connect-PnPOnline -Url $tenantAdminUrl -Interactive -ClientId $clientId
+# PowerShell 7.x is required
+# PnP module must be installed and up-to-date
+# Install   : Install-Module -Name PnP.PowerShell
+# Update    : Update-Module -Name PnP.PowerShell
+# Config Parameters
+$tenantAdminUrl = "https://m365x97415188-admin.sharepoint.com"  # Change this to your admin center URL
+$clientId = "c00e64bc-4761-4c36-b358-3119fea350e5"              # Change this to the PnP module application ID
+
+# Create a stopwatch instance to track the runtime
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
 # Get all site collections
+Connect-PnPOnline -Url $tenantAdminUrl -Interactive -ClientId $clientId
 $siteCollections = Get-PnPTenantSite
 
 # Initialize an array to store the results
 $results = @()
 
 # Exclude certain libraries
-$excludedLists = @("Form Templates", "Preservation Hold Library","Site Assets", "Pages", "Site Pages", "Images", "Site Collection Documents", "Site Collection Images","Style Library")
+$excludedLists = @("Form Templates", "Preservation Hold Library","Site Assets", "Pages", "Site Pages", "Images", 
+    "Site Collection Documents", "Site Collection Images","Style Library")
 
-# Iterate through each site collection
+# Iterate through site collections
 foreach ($site in $siteCollections){
-    Write-Host "Processing site $($site.Title)" -ForegroundColor Yellow
+    Write-Host "Processing Site: $($site.Title) ($($site.Url))" -ForegroundColor Blue
 
     # Set document library counter to 0
     $i = 0
 
-    try {
+    try{
         # Connect to the site collection
         Connect-PnPOnline -Url $site.Url -Interactive -ClientId $clientId
 
-        # Get the document libraries
+        # Get document libraries
         $documentLibraries = Get-PnPList | Where-Object { $_.BaseTemplate -eq 101 -and $_.Title -notin $excludedLists -and $_.Hidden -eq $false } # BaseTemplate 101 is for document libraries
 
+        # Iterate through document libraries
         foreach ($documentLibrary in $documentLibraries){
-            $i++
-            $Error.Clear()
+            # Display the current runtime
+            $elapsedTime = $stopwatch.Elapsed
+            Write-Host "Current Runtime: $($elapsedTime.Hours)h $($elapsedTime.Minutes)m $($elapsedTime.Seconds)s" -ForegroundColor DarkGray
 
-            Write-Host "$i/$($documentLibraries.Count): Processing document library $($site.Title)/$($documentLibrary.Title)... " -ForegroundColor Yellow -NoNewline
+            $Error.Clear()
+            
+            $i++
+            Write-Host "$i/$($documentLibraries.Count): Processing document library $($site.Title)/$($documentLibrary.Title)... " -ForegroundColor Magenta -NoNewline
 
             # Get all files in the document library
             $files = Get-PnPListItem -List $documentLibrary.Title | Where-Object { $_.FileSystemObjectType -eq "File" }
@@ -59,12 +73,17 @@ foreach ($site in $siteCollections){
             }
         }
     }catch{
-        Write-Host "Error accessing site: $($site.Url) - $_"
+        Write-Host "Error accessing site: $($site.Url) - $_" -ForegroundColor Red
     }
 }
 
+# Stop the stopwatch and display the total runtime
+$stopwatch.Stop()
+$finalTime = $stopwatch.Elapsed
+Write-Host "Total Runtime: $($finalTime.Hours)h $($finalTime.Minutes)m $($finalTime.Seconds)s"
+
 # Export results to CSV
-$outputFile = "FileVersionReport-$(Get-Date -Format yyyy.MM.dd).csv"
+$outputFile = "FileVersionCountReport-$(Get-Date -Format yyyy.MM.dd).csv"
 $results | Export-Csv -Path $outputFile -NoTypeInformation
 
 Write-Host "Output saved to $outputFile" -ForegroundColor Green
