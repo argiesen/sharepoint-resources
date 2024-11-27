2a250aa2b306f30206294c91909d56348256d45b
Andy Giesen <argiesen@gmail.com>
2024-11-27 09:41:49 -0800

    Update sharepoint-delete-file-versions.ps1

--- sharepoint-delete-file-versions.ps1
+++ sharepoint-delete-file-versions.ps1
@@ -24,7 +24,7 @@ Connect-PnPOnline -Url $tenantAdminUrl -Interactive -ClientId $clientId
 $siteCollections = Get-PnPTenantSite
 
 # Exclude certain libraries
-$excludedLists = @("Form Templates", "Preservation Hold Library","Site Assets", "Pages", "Site Pages", "Images", "Site Collection Documents", "Site Collection Images","Style Library")
+$excludedLists = @("Form Templates", "Preservation Hold Library","Site Assets", "Pages", "Site Pages", "Images", "Site Collection Documents", "Site Collection Images","Style Library", "User Photos")
 
 # Iterate through site collections
 foreach ($site in $siteCollections){
